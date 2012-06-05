e438ddb9d76764207143eec18cb87b24c8d1f94b
Art Gibson <art@embed.ly>
2012-06-04 22:00:55 -0400

    Stache it up

--- proxy/stachify.py
+++ proxy/stachify.py
@@ -12,11 +12,12 @@ import face
 
 class CleanCut(object):
 
-	def __init__(self, url, coordinates=None,):
+	def __init__(self, url, coordinates=None, mouth_size=None):
 		self.url = url
 		self.coordinates = coordinates
+		self.mouth_size = mouth_size
 		p = PIL.ImageFile.Parser()
-		raw = open('mustaches/handlebar.jpg').read()
+		raw = open('mustaches/old-smokey.png').read()
 		p.feed(raw)
 		self.mustache = p.close()
 
@@ -29,13 +30,19 @@ class CleanCut(object):
 	def place_stache(self):
 		transform_matrix = [[1,.5], [.5,1], [1,1]]
 		self.load()
-		self.mustache = self.mustache.resize(self.image.size)
-
+		#self.mustache = self.mustache.resize(self.image.size)
+		#self.mustache = self.mustache.transform(self.image.size, (1,1,self.coordinates[0], 1, 1, self.coordinates[1]))
+		ratio = 1.0 / (self.mustache.size[0] / float(self.mouth_size))
+		self.mustache = self.mustache.resize((self.mouth_size, int(self.mustache.size[1] * ratio)))
+		tl = (self.coordinates[0] - self.mustache.size[0] / 2,
+			  self.coordinates[1] - self.mustache.size[1] / 2)
+		self.image.paste(self.mustache, tl, self.mustache)
 		print self.mustache.format, self.image.mode, self.mustache.size
 		print self.image.format, self.image.mode, self.image.size
-		stache_face = PIL.ImageChops.add(self.image, self.mustache, .5, 0)
+		#stache_face = PIL.ImageChops.add(self.image, self.mustache, .5, 0)
 		stacheout = 'static/{}.jpg'.format(abs(hash(self.url)))
-		stache_face.save(stacheout)
+		#stache_face.save(self.image)
+		self.image.save(stacheout)
 		return abs(hash(self.url))
 
 def add_stache(url, mouth_center=None, roll=None, size=None):
@@ -48,5 +55,5 @@ def add_stache(url, mouth_center=None, roll=None, size=None):
     @return: local image file. 
     """
     
-    face = CleanCut(url, coordinates=mouth_center)
+    face = CleanCut(url, coordinates=mouth_center, mouth_size=size)
     return face.place_stache()
